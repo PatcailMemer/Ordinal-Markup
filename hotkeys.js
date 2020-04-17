607ad457534e83b27c6ad26fb50501012bbb179d
PatcailMemer <46659397+PatcailMemer@users.noreply.github.com>
2020-04-16 20:33:06 -0400

    Create hotkeys.js

---
+++ hotkeys.js
@@ -0,0 +1,12 @@
+const keybinds = {
+    i: () => infinity(1),
+    m: () => maxInfStuff(),
+    s: () => factorShift(1),
+    b: () => game.boostUnlock==1 ? factorBoost(1) : 0,
+    r: () => game.boostUnlock==1 ? refund() : 0,
+    c: () => game.collapseUnlock==1 ? collapse(1) : 0,
+}
+// declaring it once is probably faster
+window.onkeypress = _ => {
+  keybinds[_.key] ? keybinds[_.key]() : 0
+}
