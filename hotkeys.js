81c6aeb86cc25ea00f56c083f338cb6b5ef7f934
PatcailMemer <46659397+PatcailMemer@users.noreply.github.com>
2020-04-17 19:27:04 -0400

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -2,7 +2,7 @@ const keybinds = {
     i: () => infinity(1),
     m: () => maxInfStuff(),
     s: () => factorShift(1),
-    b: () => game.boostUnlock==1 ? factorBoost(1) : 0,
+    b: () => game.boostUnlock==1 && game.challenge==0 ? factorBoost(1) : 0,
     r: () => game.boostUnlock==1 ? refund() : 0,
     c: () => game.collapseUnlock==1 ? collapse(1) : 0,
 }
